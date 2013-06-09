ade4394fff76cb1f35bbfbf3b6ac6e2dc580ff8d
Pedro Borges <highvolt16@gmail.com>
2013-06-09 17:53:58 +0100

    font

--- modules/feupDemic/main.cs
+++ modules/feupDemic/main.cs
@@ -2,7 +2,7 @@
 
 function populateFonts()
 {      
-      %font = "Arial";      
+      %font = "Minecraftia";      
       %sizes = "18 24 32 36";      
       for (%i = 0; %i < getWordCount(%sizes); %i++)            
             populateFontCacheRange(%font, getWord(%sizes, %i), 32, 126);      
@@ -17,7 +17,7 @@ function feupDemic::create( %this )
 exec("./scripts/scenewindow.cs");
 exec("./scripts/scene.cs");
 exec("./gui/guiProfiles.cs");
-//populateFonts();
+populateFonts();
 createSceneWindow();
 createScene();
 
