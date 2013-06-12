270e98861f490e33055476936c8d3825fb3025ef
Renato Rodrigues <hi@renatorodrigues.me>
2013-06-12 23:15:36 +0100

    Map updated

--- parser/SVGMapParser/src/SVGMapParser.java
+++ parser/SVGMapParser/src/SVGMapParser.java
@@ -7,10 +7,10 @@ public class SVGMapParser {
 	public static final String INPUT_FILE = "C:\\Users\\Renato\\Dropbox\\Workspace\\DJCO\\FEUPdemic\\assets\\map\\map-picking.svg";
 	public static final String OUTPUT_FILE = "C:\\Users\\Renato\\Dropbox\\Workspace\\DJCO\\FEUPdemic\\modules\\feupDemic\\scripts\\level.cs";
 	
-	public static final int MAP_WIDTH = 1000;
+	public static final int MAP_WIDTH = 667;
 	public static final int MAP_HEIGHT = 640;
-	public static final int X_OFFSET = MAP_WIDTH / 2;
-	public static final int Y_OFFSET = MAP_HEIGHT / 2;
+	public static final double X_OFFSET = MAP_WIDTH / 2.0;
+	public static final double Y_OFFSET = MAP_HEIGHT / 2.0;
 
 	public static void main(String[] args) {
 		Parser parser = Parser.getInstance();
