b3e15cf0ddc7bae567e6599a35fd3e057715bced
Pedro Borges <highvolt16@gmail.com>
2013-06-13 02:21:44 +0100

    parser fixed

--- parser/SVGMapParser/src/Polygon.java
+++ parser/SVGMapParser/src/Polygon.java
@@ -1,14 +1,13 @@
 import java.text.DecimalFormat;
 import java.util.ArrayList;
+import java.util.Locale;
+
 
 public class Polygon {
-	private static int last_id_ = 0;
-	
-	private int id_ = last_id_++;
 	private ArrayList<Point> points_;
-	
+	static int id=0;
 	public Polygon() {
-		this(new ArrayList<Point>());
+		points_ = new ArrayList<Point>();
 	}
 	
 	public Polygon(ArrayList<Point> points) {
@@ -29,14 +28,13 @@ public class Polygon {
 	}
 
 	public String toScript() {
-		DecimalFormat df = new DecimalFormat("#.##");
+		DecimalFormat df = new DecimalFormat("#");
 
 		double area = MathHelper.polygonArea(points_);
-		double centroid_x = MathHelper.centroidX(points_, area) - SVGMapParser.X_OFFSET;
-		double centroid_y = (MathHelper.centroidY(points_, area) - SVGMapParser.Y_OFFSET) * -1;
+		double centroid_x = MathHelper.centroidX(points_, area) -SVGMapParser.X_OFFSET;
+		double centroid_y = (MathHelper.centroidY(points_, area) -SVGMapParser.Y_OFFSET) * -1;
 		
-		// TODO: Dirty hack. Needs fix. 
-		StringBuilder script = new StringBuilder(id_ + "/" + /*df.format(centroid_x)*/ 0 + " " + /*df.format(centroid_y)*/ 0 + ";");
+		StringBuilder script = new StringBuilder(df.format(id++)+"/"+df.format(centroid_x) + " " + df.format(centroid_y) + ";");
 		
 		boolean first = true;
 		for (Point point : points_) {
@@ -46,9 +44,9 @@ public class Polygon {
 				script.append(" ");
 			}
 			
-			script.append(df.format(point.x - SVGMapParser.X_OFFSET)).append(" ").append(df.format((point.y - SVGMapParser.Y_OFFSET) * -1));
+			script.append(df.format(point.x - SVGMapParser.X_OFFSET- centroid_x)).append(" ").append(df.format((point.y - SVGMapParser.Y_OFFSET  ) * -1-centroid_y));
 		}
-
+		
 		return script.toString();
 	}
 }
